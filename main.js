3b3b3e4fd4749dfa068fd32997214f14f382d0c5
Ibrokhim702 <rakhimovaziz077@gmail.com>
2025-01-17 20:24:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// const userName = prompt("Hafta kunlarini raqamda yozing (1-7)");
+
+// switch (userName) {
+//     case "1":
+//         console.log("Dushanba");
+//         break;
+//     case "2":
+//         console.log("Seshanba");
+//         break;
+//     case "3":
+//         console.log("Chorshanba");
+//         break;
+//     case "4":
+//         console.log("Payshanba");
+//         break;
+//     case "5":
+//         console.log("Juma");
+//         break;
+//     case "6":
+//         console.log("Shanba");
+//         break;
+//     case "7":
+//         console.log("Yakshanba");
+//         break;
+//     default:
+//         console.log("Xato! Haftada 7 kun bor.");
+// }
+
+
+const userName = prompt("Saon kiriting");
+
+switch(userName %2 == 0){
+    case 0:
+      console.log(`${userName} juft son`);
+      break;
+      case 
+
+}
